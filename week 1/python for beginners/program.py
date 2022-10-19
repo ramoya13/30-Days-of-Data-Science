b1758189529ba3903ba4d795d599960cf31a9a5d
ramoya <stevramoya@gmail.com>
2022-10-19 04:38:14 +0300

    update working with output

---
+++ week 1/python for beginners/program.py
@@ -0,0 +1,24 @@
+sum = 1 + 2
+print(sum)
+
+# the print() function
+print("Show this in the console")
+
+# variables
+sum = 1 + 2
+product = sum * 2
+print(product)
+
+# Data types
+#Numeric - (int, float, complex,)
+#Text type - string("a literal string")
+#Boolean - true or false
+
+planets_in_the_solar_system = 8 # pluto used to be the 9th planet, but it is too small
+distance_to_alpha_centauri = 4.367 # float, lightyears
+shuttle_landed_on_the_moon = "Apollo 11" # string
+
+# use type function to determine data type
+
+# Data Type Conversion
+# using utility function str() i.e str(date.today())
